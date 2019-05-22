88bee28c9ed1615a605854d34c87656e4fc54b01
mikel <plyx_46204@126.com>
2019-05-23 07:17:49 +0800

    add urls

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from tutorial.start import views
+from start import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
